bc856a9eff0aec47c2cd3d5e1253a1ac4d27097b
Bo Vari <bvari2890@gmail.com>
2021-10-14 13:19:10 -0400

    changing listen message

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 4088;
 
 const app = express();
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
@@ -18,5 +18,5 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 app.use('/', require("./routes"));
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}`);
+    console.log(`App running on http://localhost:${PORT}`);
 });
